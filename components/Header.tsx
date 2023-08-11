7e71305fd0cae897234438b1c853d259af1c5186
Akash Shridharan <akashshridharan2802@gmail.com>
2023-08-11 23:35:05 +0530

    Authentication modal and functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,12 @@ import { twMerge } from "tailwind-merge";
 import {RxCaretLeft, RxCaretRight} from "react-icons/rx"
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import {useSupabaseClient} from "@supabase/auth-helpers-react";
+import { FaUserAlt } from "react-icons/fa";
+import { toast } from "react-hot-toast";
+
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
 
 import Button from "./Button";
 
@@ -16,10 +22,24 @@ const Header:React.FC<HeaderProps> = ({
     children,
     className
 }) => {
+    const authModal = useAuthModal();
     const router = useRouter();
 
-    const handleLogout = () => {
-        // handle logout in future
+    const supabaseClient = useSupabaseClient();
+    const { user } = useUser();
+
+    const handleLogout = async() => {
+        const { error } = await supabaseClient.auth.signOut();
+        // reset any playing songs in future
+        router.refresh();
+
+        if(error) {
+            toast.error(error.message);
+        }
+        else{
+            toast.success('Logged Out!');
+        }
+
     }
 
   return (
@@ -56,23 +76,40 @@ const Header:React.FC<HeaderProps> = ({
                     <BiSearch className="text-black" size={20} />
                 </button>
             </div>
-            <div className="flex justify-end   items-center gap-x-4">
-                <>
-                    <div>
-                        <Button onClick={() => {}} className="
-                            bg-transparent text-neutral-300 font-medium 
-                        ">
-                            Sign up
+            <div className="flex justify-between items-center gap-x-4">
+                {user ? (
+                    <div className="flex gap-x-4 items-center">
+                        <Button 
+                            onClick={handleLogout}
+                            className="bg-white px-6 py-2"
+                        >
+                            Logout
+                        </Button>
+                        <Button 
+                            onClick={() => router.push('/account')}
+                            className="bg-white"
+                        >
+                            <FaUserAlt />
                         </Button>
                     </div>
-                    <div>
-                        <Button onClick={() => {}} className="
-                            bg-white px-6 py-2
-                        ">
-                            Log in
-                        </Button>
-                    </div>
-                </>
+                ) : (
+                    <>
+                        <div>
+                            <Button onClick={authModal.onOpen} className="
+                                bg-transparent text-neutral-300 font-medium 
+                            ">
+                                Sign up
+                            </Button>
+                        </div>
+                        <div>
+                            <Button onClick={authModal.onOpen} className="
+                                bg-white px-6 py-2
+                            ">
+                                Log in
+                            </Button>
+                        </div>
+                    </>
+                )}
             </div>
         </div>
         {children}
---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+import AuthModal from "@/components/AuthModal";
+
+const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);        
+    }, []);
+
+    if(!isMounted) {
+        return null;
+    }
+    
+
+  return (
+    <>
+        <AuthModal/>
+    </>
+  )
+}
+
+export default ModalProvider
