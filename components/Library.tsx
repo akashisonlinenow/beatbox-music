29f2f8d3cc0b0debf63f0506185918185be0e387
Akash Shridharan <akashshridharan2802@gmail.com>
2023-08-12 17:40:16 +0530

    your library section created

--- components/Library.tsx
+++ components/Library.tsx
@@ -6,8 +6,16 @@ import {AiOutlinePlus  } from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
 
-const Library = () => {
+interface LibraryProps {
+    songs: Song[]
+}
+
+const Library: React.FC<LibraryProps> = ({
+    songs
+}) => {
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
     const { user } = useUser ();
@@ -43,7 +51,13 @@ const Library = () => {
             />
         </div>
         <div className="flex flex-col gap-y-2 mt-4 px-3">
-            List of Songs!
+            {songs.map((item) => (
+                <MediaItem 
+                    onClick={() => {}}
+                    key={item.id}
+                    data={item}
+                />
+            ))}
         </div>
     </div>
   )
