0f39a98dc2c83e4daf30117e755a1d4ff5d10efe
Akash Shridharan <akashshridharan2802@gmail.com>
2023-08-13 20:28:53 +0530

    todo completed

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
+import usePlayer from "@/hooks/usePlayer";
 
 interface MediaItemProps {
     data: Song;
@@ -14,6 +15,7 @@ const MediaItem: React.FC<MediaItemProps> = ({
     data,
     onClick
 }) => {
+    const player = usePlayer();
     const imageUrl = useLoadImage(data);
 
     const handleClick = () => {
@@ -21,7 +23,7 @@ const MediaItem: React.FC<MediaItemProps> = ({
             return onClick(data.id);
         }
 
-        // TODO: Default turn on player
+        return player.setId(data.id);
     }
   return (
     <div onClick={handleClick} className="
